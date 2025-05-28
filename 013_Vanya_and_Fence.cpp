c8d6acba7915c4d1c5f39c5626647785a4012f46
learner1289 <saha2305341289@diu.edu.bd>
2025-05-29 00:31:58 +0600

    Create 013_Vanya_and_Fence.cpp

---
+++ 013_Vanya_and_Fence.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,h,width=0;
+    cin>>n>>h;
+    int arr[n];
+    for(int i=0;i<n;i++){
+    cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<=h) width++;
+        else if(arr[i]>h) width+=2;
+    }
+    cout<<width;
+}
